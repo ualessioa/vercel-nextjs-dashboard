599d5b653e007123d8118bc1632a603598b99310
AlessioLenovo <a.ardito@frankhood.it>
2026-09-19 12:39:06 +0200

    chapter 12 completed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -86,6 +86,7 @@ export async function fetchCardData() {
 }
 
 const ITEMS_PER_PAGE = 6;
+
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
@@ -160,6 +161,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+    console.log(invoice);
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ const config: Config = {
       },
     },
   },
+  // eslint-disable-next-line
   plugins: [require('@tailwindcss/forms')],
 };
 export default config;
